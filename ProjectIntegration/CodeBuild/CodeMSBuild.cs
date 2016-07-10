e1b913f58ff396d18660662d8eb88a7c6fa1dfa4
konghao <konghao8013@163.com>
2016-07-10 10:47:44 +0800

    提交多线程生成项目

--- ProjectIntegration/CodeBuild/CodeMSBuild.cs
+++ ProjectIntegration/CodeBuild/CodeMSBuild.cs
@@ -11,20 +11,20 @@ namespace CodeBuild
         public override void Build(BuildContext context)
         {
             var p = new Command();
-            p.Call(context.BuildPath + " " + context.CodePath + " /maxcpucount");
+            p.Call(context.BuildPath + " " + context.CodePath + " ");
             p.Exit();
             context.Log = p.CommadnLog;
         }
 
         public override void Publish(BuildContext context)
-        {
+        {//   /maxcpucoun
             var p = new Command();
             var ax = @"D:\KongHao\KongHao\Code\git\ProjectIntegration\ProjectIntegration\WebNetBuilder\Properties\PublishProfiles\ds.pubxml";
             var command = new StringBuilder();
             command.Append(context.BuildPath);
             command.Append(" ");
             command.Append(context.PublishItemPath);
-            command.Append(@" /t:ResolveReferences;Compile /t:_WPPCopyWebApplication  /p:Configuration=Release /maxcpucoun");
+            command.Append(@" /t:ResolveReferences;Compile /t:_WPPCopyWebApplication  /p:Configuration=Release ");
             command.Append(" /p:WebProjectOutputDir=");
             command.Append(context.publishDirectory);
             p.Call(command.ToString());
